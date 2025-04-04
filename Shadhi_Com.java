11049d74bbe0d356b6af3b43a9364914e9b905c1
Spandana66 <macharalaspandana1999@gmail.com>
2025-04-04 15:52:01 +0530

    Create Shadhi_Com.java

---
+++ Shadhi_Com.java
@@ -0,0 +1,17 @@
+package Packages;
+import java.util.*;
+
+public class Shadhi_Com
+{
+public static void main(String[] args)
+{
+	System.out.println("****Welcome to shadhi.com****");
+	Shaadi_Users u1=create();
+	Shaadi_Users u2=create();
+	Shaadi_Users u3=create();
+	System.out.println(u1);
+	System.out.println(u2);
+	System.out.println(u3);
+	System.out.println("****Thanks for visiting****");
+	
+}
